2eeae4bd6033f595782f5fd01ce9da1127ef13f1
Kothaimanjuri <107306773+Kothaimanjuri@users.noreply.github.com>
2022-06-11 17:36:37 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package week1.day1;
+
+public class Car {
+
+//	Adding comments/reference to the code
+	// To add single line comment use this
+	//	keyboard shortcut for single line class ctrl + /
+	/*To add multi line
+	comment like this use this*/
+	/*
+	 * keyboard short cut for multi line comment by selecting the lines and then click
+	   ctrl+Shift+/
+	 */
+	
+	//Variable Datatype
+	
+	byte carSeat = 5;
+	short yrManufacture = 2020;
+	int carMileage = 50;
+	long chasisNum = 112545896314L;
+	float carPrice = 1875235.4589F;
+	double regId = 1458.56891;
+	char name = 'H';
+	boolean isElectriccar = false;
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
