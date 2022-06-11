bbee004a72fff6f0af14ac944bca0f30799f6f17
Kothaimanjuri <107306773+Kothaimanjuri@users.noreply.github.com>
2022-06-11 17:46:32 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Ifelse {
+	public static void main(String[] args) {
+		int age = 18;
+		if (age < 18)
+		System.out.println("Sorry. Entry Restricted as minimum age for entry is 18");
+		else
+			System.out.println("Enjoy your time here. Wear mask and stay safe");
+	
+	}
+
+}
