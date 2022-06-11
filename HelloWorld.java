3391d8cfbdc427e33b1efb83f5985aee3deb8aa6
Kothaimanjuri <107306773+Kothaimanjuri@users.noreply.github.com>
2022-06-11 17:27:05 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("welcome to New Learning");
+//short cut to add main string is type main and ctrl+space
+//short cut to add system print is type syso and ctrl+space
+		
+	}
+
+}
